ab1f6bb41bccf7b1bff3989a0ccd657f22ebe4e8
Alasdair Monk <alasdair.monk@gmail.com>
2022-05-08 23:23:22 +0100

    Add pluralisation fn

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,41 @@ import title from "title";
 
 export default async function () {
   let nodes = figma.currentPage.selection;
-  let textNodes = nodes.filter((e)=>{return e.type == "TEXT"})
+  let textNodes = nodes.filter((e) => {
+    return e.type == "TEXT";
+  });
 
   if (textNodes.length === 0) {
-    figma.closePlugin("Select At Least 1 Text Node")
-    return
+    figma.closePlugin("Select At Least 1 Text Node");
+    return;
   }
 
   for (const node of textNodes) {
-    if (node.type == "TEXT") {
-      let nodeFonts = node.getRangeAllFontNames(0, node.characters.length);
-      loadFonts(nodeFonts).then(() => {
-        node.characters = title(node.characters);
-      });
-    }
+    formatNodeInSelection(node);
   }
 
   setTimeout(() => {
-    figma.closePlugin(`Ran Title on ${textNodes.length} Text Nodes`);
+    figma.closePlugin(
+      `Ran Title on ${textNodes.length} Text ${pluralize(
+        textNodes.length,
+        "Node"
+      )}`
+    );
   }, 500);
 }
+
+// @ts-expect-error:
+function formatNodeInSelection(node) {
+  if (node.type == "TEXT") {
+    let nodeFonts = node.getRangeAllFontNames(0, node.characters.length);
+    loadFonts(nodeFonts).then(() => {
+      node.characters = title(node.characters);
+    });
+  }
+}
+
+const pluralize = (count: number, noun: string, suffix = "s") =>
+  `${noun}${count !== 1 ? suffix : ""}`;
 
 // @ts-expect-error:
 async function loadFonts(fonts) {
